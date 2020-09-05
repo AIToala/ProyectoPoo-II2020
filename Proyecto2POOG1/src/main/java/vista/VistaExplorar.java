f0013ff5dfb7b7b13e82dbf2914d3fe5c0693161
Landívar Jerry <jlandiva@espol.edu.ec>
2020-09-04 21:38:06 -0500

    Agrego el label con la información del crater y mejoro el manejo de una excepcion al ingresar comandos inválidos.

--- Proyecto2POOG1/src/main/java/vista/VistaExplorar.java
+++ Proyecto2POOG1/src/main/java/vista/VistaExplorar.java
@@ -22,7 +22,7 @@ import javafx.scene.layout.VBox;
  * @author user
  */
 public class VistaExplorar {
-    private HBox root;
+    private VBox root = new VBox();
     private ImageView mapView;
     private Rover robot;
     
@@ -35,9 +35,9 @@ public class VistaExplorar {
             System.out.println("¡Algo salió mal al cargar el mapa!");
         }
         
-        Rover rover = new Rover();
+        robot = new Rover();
         
-        Pane espacioExplorar = new Pane(mapView, rover.roverView);
+        Pane espacioExplorar = new Pane(mapView, robot.roverView);
         espacioExplorar.setMaxSize(constantes.constantes.mapAncho, constantes.constantes.mapAlto);
         
         Label ingreseComandosLabel = new Label("Ingrese comandos:");
@@ -51,8 +51,10 @@ public class VistaExplorar {
         comandosPanel.setMaxWidth(200);
         
         
-        root = new HBox(espacioExplorar, comandosPanel);
-        root.setMaxSize(1480, 600);
+        HBox mapaComandos = new HBox(espacioExplorar, comandosPanel);
+        mapaComandos.setMaxSize(1480, 600);
+        
+        root.getChildren().add(mapaComandos);
         
         //Movimiento del robot
         comando.setOnKeyReleased((key) -> {
@@ -62,24 +64,24 @@ public class VistaExplorar {
                 Boolean escapa = false;
                 String separador ="\n";
                 try{
-                    if("sensar".equals(texto)) rover.sensar();
+                    if("sensar".equals(texto)) robot.sensar();
                     
                     else if("girar:".equals(texto.substring(0, 6))){
                         double g = Double.parseDouble(texto.substring(6));
-                        rover.girar(g);
+                        robot.girar(g);
                         
                     }else if(texto.substring(0, 8).equals("avanzar:")){
                         double d = Double.parseDouble(texto.substring(8));
-                        double x = rover.getX() + d*Math.cos(rover.getOrientacion());
-                        double y = rover.getY() + d*Math.sin(rover.getOrientacion());
+                        double x = robot.getX() + d*Math.cos(robot.getOrientacion());
+                        double y = robot.getY() + d*Math.sin(robot.getOrientacion());
                         escapa = 0 > x || x > constantes.constantes.mapAncho || 0 > y || y > constantes.constantes.mapAlto;
-                        if(!escapa) rover.avanzar(d);
+                        if(!escapa) robot.avanzar(d);
                     }else if("dirigirse:".equals(texto.substring(0, 10))){
                         String coordinates = texto.substring(10);
                         double x = Double.parseDouble(coordinates.split(",")[0]);
                         double y = Double.parseDouble(coordinates.split(",")[1]);
                         escapa = 0 > x || x > constantes.constantes.mapAncho || 0 > y || y > constantes.constantes.mapAlto;
-                        if(!escapa) rover.dirigirse(x, y);
+                        if(!escapa) robot.dirigirse(x, y);
                     }
                     else esComando = false;
                     
@@ -87,19 +89,27 @@ public class VistaExplorar {
                     
                     if (esComando){
                         comandosList.setText(texto + separador + comandosList.getText());
-                    }else System.out.println("El comando no existe");
+                    }else throw new IndexOutOfBoundsException();
                     
                     comando.clear();
-                }catch(NumberFormatException|StringIndexOutOfBoundsException e){
-                    if (e instanceof NumberFormatException)
-                        System.out.println("¡Entrada no numérica!");
-                    else System.out.println("El comando no existe");
+                }catch(NumberFormatException|IndexOutOfBoundsException e){
+                    if (e instanceof NumberFormatException){
+                        comandosList.setText("¡Entrada no numérica!\n" + comandosList.getText());
+                        comando.clear();
+                    }
+                    else {
+                        comandosList.setText(texto + " (El comando no existe)\n" + comandosList.getText());
+                        comando.clear();
+                    }
                 }
             }
         });
+        
+        Label craterInfo = new Label("Crater jaja");
+        root.getChildren().add(craterInfo);
     }
     
-    public HBox getRoot(){
+    public VBox getRoot(){
         return root;
     }
 }
